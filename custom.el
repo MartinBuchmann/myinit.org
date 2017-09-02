b1a85beefaa1132bc20ead044090c3f8d9342064
Martin Buchmann <Martin.Buchmann@googlemail.com>
2017-09-02 14:46:09 +0200

    Small adjustments for even higher Emacs user experience ;-)

--- custom.el
+++ custom.el
@@ -7,10 +7,10 @@
  '(ac-ispell-requires 4)
  '(ansi-color-names-vector
    ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#e090d7" "#8cc4ff" "#eeeeec"])
- '(custom-enabled-themes (quote (nord)))
+ '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes
    (quote
-    ("7f968c172d6ec46766773a8304c7570bdff45f1220d3700008a437d9529ca3e4" "f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" "bb749a38c5cb7d13b60fa7fc40db7eced3d00aa93654d150b9627cabd2d9b361" "2997ecd20f07b99259bddba648555335ffb7a7d908d8d3e6660ecbec415f6b95" default)))
+    ("67e998c3c23fe24ed0fb92b9de75011b92f35d3e89344157ae0d544d50a63a72" "a0dc0c1805398db495ecda1994c744ad1a91a9455f2a17b59b716f72d3585dde" "093b2a26030dcd576cad4e59b5d804bc0496e56f4e2659e8900b4814279c3402" "4a51697271e3fd202d3da73bad80d0ec5cab7e0bb1db30f79fd1d6dd6a7812dc" "7f968c172d6ec46766773a8304c7570bdff45f1220d3700008a437d9529ca3e4" "f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" "bb749a38c5cb7d13b60fa7fc40db7eced3d00aa93654d150b9627cabd2d9b361" "2997ecd20f07b99259bddba648555335ffb7a7d908d8d3e6660ecbec415f6b95" default)))
  '(fci-rule-color "#383838")
  '(nrepl-message-colors
    (quote
@@ -18,13 +18,16 @@
  '(org-modules
    (quote
     (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
+ '(osxb-import-with-timer nil)
  '(package-selected-packages
    (quote
-    (rainbow-mode rainbow-delimiters nord-theme dired-quick-sort gist web-mode osx-dictionary osx-org-clock-menubar mu4e-maildirs-extension mu4e mu4e-alert lorem-ipsum auto-complete-auctex auctex smart-comment org-beautify-theme zenburn-theme ac-math ac-ispell org-wunderlist osx-bbdb osx-lib alert readline-complete mic-paren htmlize org-ac counsel-projectile counsel-osx-app bbdb-handy bbdb org-gcal noflet w3m yasnippet which-key use-package undo-tree try powerline paredit org-bullets mode-icons markdown-mode magit ivy-hydra expand-region exec-path-from-shell elisp-slime-nav counsel command-log-mode bm beacon avy ac-slime)))
+    (wc-mode beginend pdf-tools finder+ wgrep f git-timemachine rainbow-mode rainbow-delimiters nord-theme dired-quick-sort gist web-mode osx-dictionary osx-org-clock-menubar mu4e-maildirs-extension mu4e mu4e-alert lorem-ipsum auto-complete-auctex auctex smart-comment org-beautify-theme zenburn-theme ac-math ac-ispell org-wunderlist osx-bbdb osx-lib alert readline-complete mic-paren htmlize org-ac counsel-projectile counsel-osx-app bbdb-handy bbdb org-gcal noflet w3m yasnippet which-key use-package undo-tree try powerline paredit org-bullets mode-icons markdown-mode magit ivy-hydra expand-region exec-path-from-shell elisp-slime-nav counsel command-log-mode bm beacon avy ac-slime)))
  '(pdf-view-midnight-colors (quote ("#DCDCCC" . "#383838")))
  '(safe-local-variable-values
    (quote
-    ((Syntax . Common-Lisp)
+    ((eval flyspell-mode-off)
+     (ispell-local-dictionary . de)
+     (Syntax . Common-Lisp)
      (TeX-master . t)
      (buffer-local-dictionary . "de")
      (buffer-local-dictionary . "en")
